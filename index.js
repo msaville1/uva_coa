680d7783c9efc885177f331eae619dcab71ec047
msaville1 <113384617+msaville1@users.noreply.github.com>
2023-01-25 15:36:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-import mongoose from 'mongoose'
-
-mongoose.connect("mongodb+srv://<username>:<password>@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    import mongoose from 'mongoose'
+    mongoose.connect("mongodb+srv://CACS:2htNSu0fmuqp8Itm@cluster0.ovf6qf7.mongodb.net/?retryWrites=true&w=majority";
