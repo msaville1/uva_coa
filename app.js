359a583f860880a4fe066aa33b4f1f229ac70a8c
msaville1 <113384617+msaville1@users.noreply.github.com>
2023-01-25 15:34:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,70 +4,108 @@ var server = express();
 var options =  {
   index: 'index.html'
 };
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
+var mongoose = require("mongoose");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const { format } = require("date-fns");
 
-//static files
-app.use(express.static('public'));
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
-app.use("/css", express.static(__dirname + 'public/css'));
-app.use("/js", express.static(__dirname + 'public/js'));
+var configData = require("./config/connection");
+var indexRouter = require("./routes/index");
+
+async function getApp() {
+  var connectionInfo = await configData.getConnectionInfo();
+  import mongoose from 'mongoose'
+  mongoose.connect("mongodb+srv://CACS:2htNSu0fmuqp8Itm@cluster0.ovf6qf7.mongodb.net/?retryWrites=true&w=majority";
+  var app = express();
+  var port = normalizePort(process.env.PORT || '3000');
+  app.set('port', port);
 
 //set views
-app.set('views', './views');
-app.set('view engine', 'ejs');
+  app.set('views', './views');
+  app.set("views", path.join(__dirname, "views"));
+  app.set("view engine", "ejs");
 
-const results = require('./routes/results');
-app.use('/results', results);
+//engine startup
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/about', (req, res) => {
+  app.locals.format = format;
+
+  app.use("/", indexRouter);
+  app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); // redirect bootstrap JS
+  app.use(
+    "/css",
+    express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+  // redirect CSS bootstrap
+
+  var results = require('./routes/results');
+  app.use('/results', results);
+  
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+  app.get('/about', (req, res) => {
     res.render('about', { text: 'About Page'})
-});
+  });
 
-app.get('/529Criteria', (req, res) => {
-    res.render('529Criteria', { text: '529 Criteria'})
-});
+  app.get('/529Criteria', (req, res) => {
+      res.render('529Criteria', { text: '529 Criteria'})
+  });
 
-app.get('/contact', (req, res) => {
-    res.render('contact', { text: 'Contact Page'});
-});
-//app.get('/contactSubmission', (req, res) => {
-//    res.render('contactSubmission', { text: 'Contact Submission Page'});
-    // URL at which MongoDB service is running
-//    var url = "mongodb+srv://CACS:2htNSu0fmuqp8Itm@cluster0.ovf6qf7.mongodb.net/?retryWrites=true&w=majority";
-//    var database;
-    // A Client to MongoDB
-//    var MongoClient = require('mongodb').MongoClient;
-//    var contact_json = {};
-//    contact_json["Name"] = req.query.inputName;
-//    contact_json["Email"] = req.query.inputEmail;
-//    contact_json["Details"] = req.query.details;
+  app.get('/contact', (req, res) => {
+      res.render('contact', { text: 'Contact Page'});
+  });
+  app.use(function (err, req, res, next) {
+// set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+// render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  });
+  app.get('/contactSubmission', (req, res) => {
+    res.render('contactSubmission', { text: 'Contact Submission Page'});
+// URL at which MongoDB service is running
+    import mongoose from 'mongoose'
+    mongoose.connect("mongodb+srv://CACS:2htNSu0fmuqp8Itm@cluster0.ovf6qf7.mongodb.net/?retryWrites=true&w=majority";
 
-//    MongoClient.connect(url, function(err, db) {
-//        if (err) throw err;
-        //console.log("Connected to MongoDB! (contact)");
- //       database = db.db("UVA_COA");
- //       database.collection("Contact Form Inputs").insert(contact_json, function(err, result){
- //           if(err){
- //              console.log(err);
- //           }else{
-             //   console.log(result);
-//            }
-//        });
-//        db.close();
-//    });
-//});
+  app.get('', (req, res) => {
+      res.render('index', { text: 'This is EJS'})
+  });
+  app.get('', (req, res) => {
+      res.render('base', { text: 'This is EJS'})
+  });
 
-app.get('', (req, res) => {
-    res.render('index', { text: 'This is EJS'})
-});
-app.get('', (req, res) => {
-    res.render('base', { text: 'This is EJS'})
-});
-
+  return app;
+}
 //Listen on port
 server.use('/', express.static('/home/site/wwwroot', options));
 server.listen(process.env.PORT);
 
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+ function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+module.exports = {
+  getApp
+};
